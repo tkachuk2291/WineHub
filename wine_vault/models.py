6d145ad57bd39821f60b04ef29477404677e61f2
Anton Tkachuk <tkacuk2291@gmail.com>
2024-05-31 16:50:04 +0300

    created models for app wine_vault

--- wine_vault/models.py
+++ wine_vault/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Winery(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Location(models.Model):
+    country = models.CharField(max_length=100)
+    region = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.country} · {self.region}"
+
+
+class Rating(models.Model):
+    average = models.DecimalField(max_digits=3, decimal_places=2)
+    reviews = models.PositiveIntegerField()
+    wine = models.ForeignKey("Wine", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.average} ({self.reviews} reviews)"
+
+
+class Wine(models.Model):
+    name = models.CharField(max_length=255)
+    vintage = models.PositiveIntegerField()
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    image_url = models.URLField()
+
+    def __str__(self):
+        return f"{self.name} ({self.vintage})"
+
