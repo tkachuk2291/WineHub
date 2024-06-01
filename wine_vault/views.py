2845b2d10b68790504beede5b017ef7e82b53b18
Anton Tkachuk <tkacuk2291@gmail.com>
2024-06-01 23:59:58 +0300

    implemented filtering and searching by query params

--- wine_vault/views.py
+++ wine_vault/views.py
@@ -1,13 +1,44 @@
 from rest_framework import viewsets
-
 from wine_vault.models import Wine
 from wine_vault.serializers import WinesSerializer
 
 
-class AllWinesViewSet(viewsets.ModelViewSet):
-    queryset = Wine.objects.all()
-    model = Wine
-    serializer_class = WinesSerializer
+def filtering_query(type_wine_filtering, params):
+    queryset = Wine.objects.filter(wine_type__type=type_wine_filtering)
+    name = params.request.query_params.get("name")
+    country = params.request.query_params.get("country")
+    region = params.request.query_params.get("region")
+    average = params.request.query_params.get("average")
+    reviews = params.request.query_params.get("reviews")
+    vintage = params.request.query_params.get("vintage")
+
+    if name:
+        queryset = queryset.filter(
+            name__icontains=name
+        )
+    if country:
+        queryset = queryset.filter(
+            location__country__icontains=country
+        )
+    if region:
+        queryset = queryset.filter(
+            location__region__icontains=region
+        )
+
+    if average:
+        queryset = queryset.filter(
+            rating__average__icontains=average
+        )
+    if reviews:
+        queryset = queryset.filter(
+            rating__reviews__icontains=reviews
+        )
+    if vintage:
+        queryset = queryset.filter(
+            vintage=vintage
+        )
+
+    return queryset.distinct()
 
 
 class RedWineViewSet(viewsets.ModelViewSet):
@@ -15,8 +46,7 @@ class RedWineViewSet(viewsets.ModelViewSet):
     serializer_class = WinesSerializer
 
     def get_queryset(self):
-        queryset = Wine.objects.all().filter(wine_type__type="red")
-
+        return filtering_query(type_wine_filtering="red", params=self)
 
 
 class WhiteWineViewSet(viewsets.ModelViewSet):
@@ -24,7 +54,7 @@ class WhiteWineViewSet(viewsets.ModelViewSet):
     serializer_class = WinesSerializer
 
     def get_queryset(self):
-        return Wine.objects.all().filter(wine_type__type="white")
+        return filtering_query(type_wine_filtering="white", params=self)
 
 
 class SparklingWineViewSet(viewsets.ModelViewSet):
@@ -32,7 +62,7 @@ class SparklingWineViewSet(viewsets.ModelViewSet):
     serializer_class = WinesSerializer
 
     def get_queryset(self):
-        return Wine.objects.all().filter(wine_type__type="sparkling")
+        return filtering_query(type_wine_filtering="sparkling", params=self)
 
 
 class RoseWineViewSet(viewsets.ModelViewSet):
@@ -40,7 +70,7 @@ class RoseWineViewSet(viewsets.ModelViewSet):
     serializer_class = WinesSerializer
 
     def get_queryset(self):
-        return Wine.objects.all().filter(wine_type__type="rose")
+        return filtering_query(type_wine_filtering="rose", params=self)
 
 
 class DessertWineViewSet(viewsets.ModelViewSet):
@@ -48,7 +78,7 @@ class DessertWineViewSet(viewsets.ModelViewSet):
     serializer_class = WinesSerializer
 
     def get_queryset(self):
-        return Wine.objects.all().filter(wine_type__type="dessert")
+        return filtering_query(type_wine_filtering="dessert", params=self)
 
 
 class PortWineViewSet(viewsets.ModelViewSet):
@@ -56,4 +86,52 @@ class PortWineViewSet(viewsets.ModelViewSet):
     serializer_class = WinesSerializer
 
     def get_queryset(self):
-        return Wine.objects.all().filter(wine_type__type="port")
+        return filtering_query(type_wine_filtering="port", params=self)
+
+
+class AllWinesViewSet(viewsets.ModelViewSet):
+    model = Wine
+    serializer_class = WinesSerializer
+    Wine.objects.all()
+
+    def get_queryset(self):
+        queryset = Wine.objects.all()
+        name = self.request.query_params.get("name")
+        country = self.request.query_params.get("country")
+        region = self.request.query_params.get("region")
+        average = self.request.query_params.get("average")
+        reviews = self.request.query_params.get("reviews")
+        vintage = self.request.query_params.get("vintage")
+        wine_type = self.request.query_params.get("wine_type")
+
+        if name:
+            queryset = queryset.filter(
+                name__icontains=name
+            )
+        if country:
+            queryset = queryset.filter(
+                location__country__icontains=country
+            )
+        if region:
+            queryset = queryset.filter(
+                location__region__icontains=region
+            )
+
+        if average:
+            queryset = queryset.filter(
+                rating__average__icontains=average
+            )
+        if reviews:
+            queryset = queryset.filter(
+                rating__reviews__icontains=reviews
+            )
+        if vintage:
+            queryset = queryset.filter(
+                vintage=vintage
+            )
+        if wine_type:
+            queryset = queryset.filter(
+                wine_type__type__icontains=wine_type
+            )
+
+        return queryset.distinct()
